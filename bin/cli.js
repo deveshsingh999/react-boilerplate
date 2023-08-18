8bf8e7356edaf0650547e4226703e9b22ad6bb6e
deveshps <deveshps@dewsolutions.in>
2023-08-18 10:28:24 +0530

    added

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
-// const path = require('path');
-// const fs = require('fs');
 
 const runCommand = command => {
     try {
@@ -14,20 +12,10 @@ const runCommand = command => {
     return true
 }
 
-// if (process.argv.length < 3) {
-//     console.log('You have to provide a name to your app.');
-//     console.log('For example :');
-//     console.log('    npx create-my-boilerplate my-app');
-//     process.exit(1);
-// }
-
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/deveshps/react-boilerplate.git ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
-// const currentPath = process.cwd();
-// const projectPath = path.join(currentPath, projectName);
-// const git_repo = YOUR_GIT_URL;
 
 console.log("Cloning the repository with name ",repoName);
 const checkOut = runCommand(gitCheckoutCommand);
@@ -44,37 +32,3 @@ console.log("Congratulations! You are ready. Follow the following commands to st
 console.log(`cd ${repoName} && npm start`);
 
 //npm publish --access=public
-
-
-// try {
-//     fs.mkdirSync(projectPath);
-//   } catch (err) {
-//     if (err.code === 'EEXIST') {
-//       console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
-//     } else {
-//       console.log(error);
-//     }
-//     process.exit(1);
-//   }
-
-//   async function main() {
-//     try {
-//       console.log('Downloading files...');
-//       execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
-
-//       process.chdir(projectPath);
-
-//       console.log('Installing dependencies...');
-//       execSync('npm install');
-
-//       console.log('Removing useless files);
-//       execSync('npx rimraf ./.git');
-//       fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true});
-
-//       console.log('The installation is done, this is ready to use !');
-
-//     } catch (error) {
-//       console.log(error);
-//     }
-// }
-// main();
--- src/App.js
+++ src/App.js
@@ -1,5 +1,65 @@
-const App = () => {
-    return <div>React Boiler Plate with script :- .</div>
+import {useMemo,useRef,useCallback,useEffect,useState,useContext,useReducer} from "react"
+import * as React from "react"
+const initialValue = {user:{name:"Devesh"},other:{name:"singh"}}
+
+const UserContext = React.createContext(initialValue.user)
+
+const OtherContext = React.createContext(initialValue.other)
+
+const reducer = (state,action) => {
+    switch(action){
+        case "inc": return {...state,count:state?.count + 1}
+        case "dec" : return {...state,count:state?.count -1}
+        default : return {...state}
+    }
 }
 
+const a = ""
+
+const useToast = () => {
+    const [value,setValue] = useState(a.value)
+
+    useEffect(() => {
+        const s = a.subscribe((v) =>setValue(v));
+        return () => s.unsubscribe()
+    })
+
+    return [value]
+}
+
+
+const App = React.memo(() => {
+    const [count,setCount] = useState()
+    const [value,dispatch] = useReducer(reducer,initialValue)
+    const nameRef = useRef(null)
+    const avalue = useMemo((value) => {return value/2},[count])
+    const aCom = useCallback(() => {},[])
+    const [a] = useToast();
+    useEffect(() => {
+        dispatch("inc")
+        const foo = async() => {
+            await fetch("").then(e => e.json()).then(e => console.log(e)).catch(e => console.error(e))
+        }
+        foo()
+        let s = window.addEventListener('mousemove',() => {})
+
+        return () => {window.removeEventListener("mousemove",() => {})}
+    },[])
+
+    return <>
+        <div>React Boiler Plate with script :- .</div>
+        <UserContext.Provider value={initialValue.user} >
+            <OtherContext.Provider value={initialValue.other}>
+                <Child />
+            </OtherContext.Provider>
+        </UserContext.Provider>
+    </>
+})
+const Child = () => {
+    const user = useContext(UserContext)
+    const ot = useContext(OtherContext)
+    return <div>
+      {user?.name} + {ot?.name}
+    </div>
+}
 export default App
